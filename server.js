257a3e5bacae8b84306d0869234d237bd795ae3b
enze-l <leon.enzenberger@protonmail.com>
2022-04-19 13:51:49 +0200

    Automatically figure out display-output name

--- server.js
+++ server.js
@@ -2,42 +2,45 @@ const shell = require('shelljs')
 const asciichart = require('asciichart')
 const axios = require('axios')
 
-const serverAdress = "http://192.168.2.64:50000"
+const serverAddress = "http://192.168.2.64:50000"
+const displayBrightness = 1
 
-function getHundretValues(){
-    return axios.get(serverAdress + "/list/day")
+function getHundredValues(){
+    return axios.get(serverAddress + "/list/100")
 }
 
 function getMinValue(){
-    return axios.get(serverAdress + "/reading/min")
+    return axios.get(serverAddress + "/reading/min")
 }
 
 function getMaxValue(){
-    return axios.get(serverAdress + "/reading/max")
+    return axios.get(serverAddress + "/reading/max")
 }
 
 function getCurrentValue(){
-    return axios.get(serverAdress + "/reading")
+    return axios.get(serverAddress + "/reading")
 }
 
-Promise.all([getHundretValues(), getMinValue(), getMaxValue(), getCurrentValue()])
+Promise.all([getHundredValues(), getMinValue(), getMaxValue(), getCurrentValue()])
     .then(function(results) {
-        const hundretValues = results[0].data
+        const hundredValues = results[0].data
         const minValue = results[1].data
         const maxValue = results[2].data
         const currentValue = results[3].data
 
-        const readingLine = String(hundretValues).split(/(\s+)/).filter( e => e.trim().length > 0)
+        const readingLine = String(hundredValues).split(/(\s+)/).filter( e => e.trim().length > 0)
         const baseLine = new Array(readingLine.length).fill(String(minValue))
         const topLine = new Array(readingLine.length).fill(String(maxValue))
         const currentLine = new Array(readingLine.length).fill(String(currentValue))
 
-        config = {
+        const config = {
             colors: [ asciichart.blue, asciichart.default, asciichart.yellow, asciichart.green ],
             height: 20
         }
 
-        shell.exec("xrandr --output DP-0 --brightness 1")
+        const displayInfoArray = String(shell.exec("xrandr --prop")).split(/(\s+)/).filter( e => e.trim().length > 0)
+        const displayName = displayInfoArray[displayInfoArray.indexOf("connected") - 1]
+        shell.exec("xrandr --output " + displayName + " --brightness " + displayBrightness)
 
         console.log(asciichart.plot([baseLine, readingLine, topLine, currentLine], config))
     })
