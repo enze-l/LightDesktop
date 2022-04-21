7c8d8c488335226821ffe86f6cacd7e8069aea92
enze-l <leon.enzenberger@protonmail.com>
2022-04-21 12:12:28 +0200

    Add getter for min and max brightness

--- server.js
+++ server.js
@@ -25,53 +25,61 @@ function sendSuccess(res){
     res.sendStatus(204)
 }
 
-app.get('/sensor/day', async (req, res) => {
-    const response = await axios.get(serverAddress + "/list/day")
-    res.send(response.data);
-    console.log("get /sensor/day value: " + response.data)
-})
-
-app.get('/sensor/100', async (req, res) => {
-    const response = await axios.get(serverAddress + "/list/100")
-    res.send(response.data);
-    console.log("get /sensor/100 value: " + response.data)
-})
-
+//display brightness
 app.post('/display/brightness',(req, res) =>{
     setBrightness(req.body)
-    console.log("post /display/brightness value: " + req.body)
     sendSuccess(res)
+    console.log("post /display/brightness value: " + req.body)
 })
-
 app.get('/display/brightness',(req, res) =>{
     res.send(String(display_brightness));
     console.log("get /display/brightness value: " + display_brightness)
 })
 
+//display min
 app.post('/display/min',(req, res) =>{
     display_min = req.body
-    console.log("post /display/min value: " + req.body)
     sendSuccess(res)
+    console.log("post /display/min value: " + req.body)
+})
+app.get('/display/min',(req, res) =>{
+    res.send(String(display_min));
+    console.log("get /display/brightness value: " + display_min)
 })
 
+//display max
 app.post('/display/max', (req, res) => {
     display_max = req.body
     console.log("post /display/max accessed value: " + req.body)
     sendSuccess(res)
 })
+app.get('/display/max',(req, res) =>{
+    res.send(String(display_max));
+    console.log("get /display/brightness value: " + display_max)
+})
 
+//sensor values
 app.get('/sensor/max', async (req, res) => {
     const response = await axios.get(serverAddress + "/reading/max")
     const max_value = String(response.data)
     console.log("get /sensor/max value: " + max_value)
     res.send(max_value)
 })
-
 app.get('/sensor', async (req, res) =>{
     const response = await axios.get(serverAddress + "/reading")
     const current_value = String(response.data)
     console.log("get /sensor value: " + current_value)
     res.send(current_value)
+})
+app.get('/sensor/day', async (req, res) => {
+    const response = await axios.get(serverAddress + "/list/day")
+    res.send(response.data);
+    console.log("get /sensor/day value: " + response.data)
+})
+app.get('/sensor/100', async (req, res) => {
+    const response = await axios.get(serverAddress + "/list/100")
+    res.send(response.data);
+    console.log("get /sensor/100 value: " + response.data)
 })
 
 app.listen(port, '127.0.0.1', () => console.log(`Listening on port ${port}..`))
